bfb5b20308b8bc4bd109f94b2965e3e70d907140
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-05-23 23:33:58 +0900

    enter-key

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -71,6 +71,13 @@ export default function SignIn({ setName }) {
             autoComplete="email"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              console.log({ key: e.key });
+              if (e.key === 'Enter') {
+                setName(e.target.value);
+                e.preventDefault();
+              }
+            }}
           />
           <Button
             type="button"
