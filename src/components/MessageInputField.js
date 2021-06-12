54dfe98d249313a8138e8281d408d4a9f990d745
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-06-12 20:26:08 +0900

    create submit button component

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import { gravatarPath } from '../gravatar'
 import MessageField from './MessageField';
+import MessageSubmitButton from './MessageSubmitButton';
 
 console.log(process.env.REACT_APP_FOO);
 
@@ -28,7 +29,9 @@ const MessageInputField = ({ name }) => {
         <Grid item xs={10}>
           <MessageField name={name} setText={setText} text={text} />
         </Grid>
-        <Grid item xs={1}>ボタン</Grid>
+        <Grid item xs={1}>
+          <MessageSubmitButton name={name} setText={setText} text={text}/>
+        </Grid>
       </Grid>  
     </div>
   );
